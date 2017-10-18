dcdef52ae266eb9b784f13b8a352f5d64c77171b
CHEN <seagullbird823@gmail.com>
2017-10-18 10:28:41 +0800

    added powerline fonts installation

--- Ubuntu/init.sh
+++ Ubuntu/init.sh
@@ -23,6 +23,12 @@ function InstallOhMyZsh()
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     # geometry theme
     git clone https://github.com/geometry-zsh/geometry.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/geometry
+    # powerline fonts
+    git clone https://github.com/powerline/fonts.git --depth=1
+    cd fonts
+    ./install.sh
+    cd ..
+    rm -rf fonts
 }
 
 function InstallGit()
