92f72922ad7b29f4baf6584acc4af15656bb9f5a
CHEN <seagullbird823@gmail.com>
2017-12-11 10:44:06 +0800

    Create config_zsh.sh

---
+++ Ubuntu/config_zsh.sh
@@ -0,0 +1,17 @@
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# geometry theme
+git clone https://github.com/geometry-zsh/geometry.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/geometry
+
+# powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# zshrc
+cp .zshrc ${HOME}
+cd ${HOME}
+source .zshrc
